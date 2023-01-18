52b9dfe3bed1fbfb4e26f79a9cbf8e40986f6015
Henna Singh <hennasingh04@gmail.com>
2023-01-18 16:56:55 +0000

    Immplemented Locator Activity to enter location or cuisine to locate for

---
+++ app/src/main/java/mongodb/mobile/foodfinder/FoodLocationList.kt
@@ -0,0 +1,11 @@
+package mongodb.mobile.foodfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FoodLocationList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.food_location_list)
+    }
+}
--- app/src/main/java/mongodb/mobile/foodfinder/LocatorActivity.kt
+++ app/src/main/java/mongodb/mobile/foodfinder/LocatorActivity.kt
@@ -1,11 +1,55 @@
 package mongodb.mobile.foodfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import io.realm.mongodb.User
+import mongodb.mobile.foodfinder.databinding.ActivityLocatorBinding
 
 class LocatorActivity : AppCompatActivity() {
+
+    private lateinit var locatorBinding: ActivityLocatorBinding
+    private var user: User? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_locator)
+        locatorBinding = ActivityLocatorBinding.inflate(layoutInflater)
+        setContentView(locatorBinding.root)
+
+        user = foodFinderApp.currentUser()
+
+        locatorBinding.btnSearch.setOnClickListener {
+            validateAndSearch()
+        }
+        title = "Food Finder in New York"
+    }
+
+    private fun validateAndSearch() {
+        if(!credentialsValid()){
+            fieldsEmptyError("Both fields cannot be empty")
+            return
+        } else {
+            val location = locatorBinding.etlocation.text.toString()
+            val food = locatorBinding.etFood.text.toString()
+
+            //once validated, pass the values to List Activity to display
+
+            val intent = Intent(applicationContext, FoodLocationList::class.java)
+            intent.putExtra("LOCATION", location)
+            intent.putExtra("FOOD", food)
+            startActivity(intent)
+
+        }
+    }
+
+    private fun fieldsEmptyError(errorMsg: String) {
+
+        Toast.makeText(baseContext, errorMsg, Toast.LENGTH_LONG).show()
+    }
+
+    private fun credentialsValid(): Boolean = when {
+        locatorBinding.etFood.text.toString().isEmpty() && locatorBinding.etlocation.text.isEmpty() -> false
+        else-> true
     }
 }
